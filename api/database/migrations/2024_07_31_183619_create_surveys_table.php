28df2a0e8000e72d56133a853d5ebb7ce8226ed1
AnthonyBeckett <antneyb10@gmail.com>
2024-07-31 19:54:35 +0100

    created migrations for tables we need with factories to use in testing later on

---
+++ api/database/migrations/2024_07_31_183619_create_surveys_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('surveys', function (Blueprint $table): void {
+            $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->string('title', 1000);
+            $table->string('slug', 1000);
+            $table->tinyInteger('status');
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->timestamp('expire_date')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('surveys');
+    }
+};
